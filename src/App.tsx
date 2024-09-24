cf4bc7871753aa697cfaa53651379f4de35218d2
ZAP <juzapelini@gmail.com>
2024-09-24 16:16:13 -0300

    layout fullscreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './modules/login/LoginPage';
 import Main from './modules/main/MainPage';
